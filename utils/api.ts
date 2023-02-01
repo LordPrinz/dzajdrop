8bd0016d1370889d7e73bb6deea2da3d3dd2c8a6
LordPrinz <plazinski2902@gmail.com>
2023-02-01 21:50:03 +0100

    Add save method

--- utils/api.ts
+++ utils/api.ts
@@ -1,24 +1,30 @@
 import { Error, Success } from "../interfaces/api";
 
-export const upload = async (file: File): Promise<Success | Error> => {
+export const upload = async (
+	file: File
+): Promise<Success | Error | Response> => {
 	const endpoint = "https://api.anonfiles.com/upload";
 
 	const body = new FormData();
 
 	body.append("file", file);
 
-	return (await fetch(endpoint, {
-		method: "POST",
-		body,
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "multipart/form-data",
-		},
-	})) as any;
+	return (await (
+		await fetch(endpoint, {
+			method: "POST",
+			body,
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "multipart/form-data",
+			},
+		})
+	).json()) as any;
 };
 
-export const getInfo = async (id: string): Promise<Success | Error> => {
+export const getInfo = async (
+	id: string
+): Promise<Success | Error | Response> => {
 	const endpoint = `https://api.anonfiles.com/v2/file/${id}/info`;
 
-	return (await fetch(endpoint)) as any;
+	return (await (await fetch(endpoint)).json()) as any;
 };
---
+++ utils/database.ts
@@ -0,0 +1,24 @@
+import { upload } from "./api";
+
+type data = {
+	id: string;
+	path?: string;
+};
+
+export const save = async (file: File, data: data) => {
+	if (!file) {
+		throw new Error("No file provided!");
+	}
+
+	if (!data?.id) {
+		throw new Error("No id provided!");
+	}
+
+	const response = await upload(file);
+
+	if (!response.status) {
+		throw new Error((response as any).error.message);
+	}
+
+	// save to mongo
+};
