d934d52055af8a95b65f9cbeb0c36dc607962b08
LordPrinz <plazinski2902@gmail.com>
2023-02-01 21:31:48 +0100

    Create error types

--- interfaces/api.ts
+++ interfaces/api.ts
@@ -19,3 +19,40 @@ export type SuccessStatus = {
 		file: File;
 	};
 };
+
+export type ErrorType =
+	| "ERROR_FILE_NOT_PROVIDED"
+	| "ERROR_FILE_EMPTY"
+	| "ERROR_FILE_INVALID"
+	| "ERROR_USER_MAX_FILES_PER_HOUR_REACHED"
+	| "ERROR_USER_MAX_FILES_PER_DAY_REACHED"
+	| "ERROR_USER_MAX_BYTES_PER_HOUR_REACHED"
+	| "ERROR_USER_MAX_BYTES_PER_DAY_REACHED"
+	| "ERROR_FILE_DISALLOWED_TYPE"
+	| "ERROR_FILE_SIZE_EXCEEDED"
+	| "ERROR_FILE_BANNED"
+	| "STATUS_ERROR_SYSTEM_FAILURE"
+	| "FILE_NOT_FOUND";
+
+export type ErrorCode =
+	| 10
+	| 11
+	| 12
+	| 20
+	| 21
+	| 22
+	| 23
+	| 30
+	| 31
+	| 32
+	| 40
+	| 404;
+
+export type ErrorStatus = {
+	status: boolean;
+	error: {
+		message: string;
+		type: ErrorType;
+		code: ErrorCode;
+	};
+};
