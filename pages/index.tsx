5c989c31a4646915fa3c2fd64aac9ca7f264e73a
LordPrinz <plazinski2902@gmail.com>
2023-02-01 12:02:19 +0100

    Configure project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
 
+import '../styles/index.css'
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
+<div>xd</div>
 )
 
 export default IndexPage
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+      screens: {
+          tablet: { max: "650px" },
+          smallDesk: { max: "800px" },
+      },
+      keyframes: {
+          fillEgg: {
+              "0%": {
+                  fill: "#414141",
+              },
+              "100%": {
+                  fill: "#4762fb",
+              },
+          },
+          fillCircle: {
+              "0%": {
+                  "stroke-dashoffset": 440,
+              },
+              "100%": {
+                  "stroke-dashoffset": 0,
+              },
+          },
+          fadeIn: {
+              "0%": {
+                  transform: "translateY(calc(-50% + 20px)) translateX(-50%)",
+                  opacity: 0,
+              },
+              "100%": {
+                  transform: "translateY(-50%) translateX(-50%)",
+                  opacity: 1,
+              },
+          },
+      },
+      extend: {
+          animation: {
+              "fill-circle": "fillCircle .7s linear .5s  1 both",
+              "fill-egg": "fillEgg .2s linear 1.3s 1 both",
+              fadeIn: "fadeIn .25s linear 1.3s 1 both",
+          },
+      },
+  },
+  plugins: [],
+};
