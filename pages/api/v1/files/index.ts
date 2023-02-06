5841b8231db5268e82fe666be2d84ab5a386d0a1
LordPrinz <plazinski2902@gmail.com>
2023-02-06 09:42:53 +0100

    Add api types

--- pages/api/v1/files/index.ts
+++ pages/api/v1/files/index.ts
@@ -1,1 +1,12 @@
-export default async function handler() {}
+import { NextApiRequest, NextApiResponse } from "next";
+import rateLimit from "../../../../utils/rateLimit";
+
+const limiter = rateLimit({
+  interval: 1000,
+  uniqueTokenPerInterval: 500,
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {}
