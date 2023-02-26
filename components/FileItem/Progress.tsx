8038331bb06177e416dcf829eeebd13e0dd8feba
LordPrinz <plazinski2902@gmail.com>
2023-02-26 17:14:38 +0100

    Create background progress

--- components/FileItem/Progress.tsx
+++ components/FileItem/Progress.tsx
@@ -1,3 +1,19 @@
 export default ({ progress }) => {
-	return <li className="file">Progress</li>;
+	return (
+		<li className="file">
+			<div
+				className={`absolute top-0 -left-full w-full h-full bg-main-lighterGrey -z-10 transition translate-x-[${progress}%]`}
+			></div>
+			<div className="flex justify-between items-center">
+				<div>
+					<div className="text-sm mb-1 font-bold">Uploading...</div>
+					<div className="text-xs font-normal text-main-grey">
+						{progress}% &#183; 10 seconds left
+					</div>
+				</div>
+				<div>PAUSE</div>
+			</div>
+			<div></div>
+		</li>
+	);
 };
