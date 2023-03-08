52a3ab352a821d12068f8b5b6e2714b7da397b01
LordPrinz <plazinski2902@gmail.com>
2023-03-08 13:39:05 +0100

    Add db Connect heloper

--- pages/api/v1/files/index.ts
+++ pages/api/v1/files/index.ts
@@ -1,7 +1,6 @@
 import { IncomingForm } from "formidable";
 import { NextApiHandler } from "next";
-import { upload, download } from "node-annonfiles";
-import fs from "fs";
+import { upload } from "node-annonfiles";
 import path from "path";
 import { saveFile } from "../../../../utils/database";
 import { generateShortLink } from "../../../../utils/shorter";
@@ -13,47 +12,56 @@ export const config = {
 };
 
 const handler: NextApiHandler = async (req, res) => {
-	const form = new IncomingForm();
+	if (req.method === "POST") {
+		const form = new IncomingForm();
 
-	form.on("fileBegin", (name, file) => {
-		const newPath = file.filepath.slice(0, -25);
-		file.filepath = path.join(newPath, file.originalFilename);
-	});
+		form.on("fileBegin", (name, file) => {
+			const newPath = file.filepath.slice(0, -25);
+			file.filepath = path.join(newPath, file.originalFilename);
+		});
 
-	form.parse(req, async (err, fields, files) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-
-		const path = (files.file as any).filepath;
-
-		try {
-			const fileData = await upload({
-				file: path,
-				key: null,
-			});
-			if (!fileData.status) {
-				throw new Error("Upload failed!");
+		form.parse(req, async (err, fields, files) => {
+			if (err) {
+				console.error(err);
+				return;
 			}
-			console.log(fileData);
 
-			const fullLink = fileData.data.file.url.short;
+			const path = (files.file as any).filepath;
 
-			res.send({
-				fullLink,
-			});
+			try {
+				const fileData = await upload({
+					file: path,
+					key: null,
+				});
+				if (!fileData.status) {
+					throw new Error("Upload failed!");
+				}
+				console.log(fileData);
 
-			const shortLink = generateShortLink(fullLink);
+				const fullLink = fileData.data.file.url.short;
 
-			const response = await saveFile({
-				fullLink,
-				shortLink: "",
-			});
-		} catch (err) {
-			console.error(err);
-		}
-	});
+				const shortLink = generateShortLink(fullLink);
+
+				console.log(shortLink);
+
+				// const response = await saveFile({
+				// 	fullLink,
+				// 	shortLink,
+				// });
+
+				return res.status(201).send({
+					url: shortLink,
+				});
+			} catch (err) {
+				return console.error(err);
+			}
+		});
+	}
+
+	// const url = `${req.headers.host}/404`;
+	// const fetchRes = await fetch(`https://${url}`);
+	// const notFoundPage = await fetchRes.text();
+	// return res.status(404).send(notFoundPage);
 };
 
 export default handler;
