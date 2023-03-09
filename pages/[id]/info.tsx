eeeb43e2a0a74b8b0b453c881b3d2df2c1ff2d55
LordPrinz <plazinski2902@gmail.com>
2023-03-09 21:25:44 +0100

    Add download button

--- pages/[id]/info.tsx
+++ pages/[id]/info.tsx
@@ -1,6 +1,7 @@
 import type { NextPage, GetServerSideProps } from "next";
 import Head from "next/head";
-import { getInfo } from "node-annonfiles";
+
+import { download, getInfo } from "node-annonfiles";
 import FileIcon from "../../components/FileItem/FileIcon";
 import dbConnect from "../../lib/dbConnect";
 import fileSchema from "../../models/file-schema";
@@ -8,29 +9,29 @@ import fileSchema from "../../models/file-schema";
 type Props = {
 	size: string;
 	name: string;
-	id: string;
+	downloadLink: string;
 	extension: string;
 };
 
 const StatsPage: NextPage<Props> = (props) => {
-	const downloadClickHandler = () => {
-		console.log("XD");
+	const downloadClickHandler = async () => {
+		location.href = props.downloadLink;
 	};
 
 	return (
 		<>
 			<Head>
-				<title>{props.name} | info</title>
+				<title>File info</title>
 			</Head>
 			<div className="file-container">
-				<div className="items-center gap-10 flex rounded-r-full">
+				<div className="items-center  flex rounded-r-full rounded-l-lg">
 					<FileIcon extension={props.extension} />
-					<span>{props.name}</span>
-					<span>{props.size}</span>
-					<span>{props.extension}</span>
+					<span className=" pr-4 pl-8">{props.name}</span>
+					<span className=" pr-4 pl-4">{props.size}</span>
+					<span className="uppercase pl-3 pr-3">{props.extension}</span>
 				</div>
 				<button
-					className="ml-3 text-[#f8f8f8] pr-6 py-4 rounded-r-full  pl-12 bg-[#4762FB] overflow-hidden relative download-button"
+					className="ml-3 text-[#f8f8f8] pr-6 py-5 rounded-r-full  pl-12 bg-[#4762FB] overflow-hidden relative download-button"
 					onClick={downloadClickHandler}
 				>
 					Download
@@ -59,17 +60,19 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 
 	const fetchedData = await getInfo(fileId);
 
-	const { name, id, size } = fetchedData.data.file.metadata;
+	const { name, size } = fetchedData.data.file.metadata;
 
 	const extensionParts = name.split("_");
 
 	const extension = extensionParts[extensionParts.length - 1];
 
+	const downloadLink = await download(fileId);
+
 	return {
 		props: {
 			size: size.readable,
 			name,
-			id,
+			downloadLink,
 			extension,
 		},
 	};
