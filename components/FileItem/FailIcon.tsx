ab372f61bb76eee611397d38281ed19f3c245b48
LordPrinz <plazinski2902@gmail.com>
2023-03-10 15:28:29 +0100

    Style failIcon

--- components/FileItem/FailIcon.tsx
+++ components/FileItem/FailIcon.tsx
@@ -11,13 +11,13 @@ export default () => {
 
 	return (
 		<div
-			className="rounded-full bg-[#d9f0e6] p-2 text-[#e6f8f0]"
+			className="rounded-full bg-[#FFCDD2] p-2 text-[#e6f8f0]"
 			style={{ opacity, transition: "0.3s" }}
 		>
-			<div className="w-5 h-5 border-2 border-[#53e685] rounded-full relative">
-				<div className="w-0.5 h-3.5 bg-[#53e685] left-1/2 top-0 translate-x-1 -translate-y-0.5 absolute rotate-[35deg] z-10"></div>
-				<div className="w-1.5 h-3.5 bg-[#d9f0e6] left-1/2 top-0.5 translate-x-[5px] -translate-y-[8px] absolute rotate-[35deg] z-0"></div>
-				<div className="w-0.5 h-1.5 bg-[#53e685] left-1/2 top-1/2 -translate-x-[2px] -translate-y-1/2 absolute rotate-[125deg] z-0"></div>
+			<div className="w-5 h-5 border-2 border-[#F44336] rounded-full p-  relative">
+				<div className="w-0.5 h-[17px] bg-[#F44336] left-1/2 top-0 translate-x-0  -translate-y-0.5 absolute rotate-[45deg] z-10"></div>
+				<div className="w-1.5 h-2.5 bg-[#FFCDD2] left-1/2 top-0.5 translate-x-[3px] -translate-y-[5px] absolute rotate-[35deg] z-0"></div>
+				<div className="w-0.5 h-3.5 bg-[#F44336] left-1/2 top-1/2 -translate-x-[1px] -translate-y-1/2 absolute rotate-[135deg] z-0"></div>
 			</div>
 		</div>
 	);
