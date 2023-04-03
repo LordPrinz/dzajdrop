1083baa3b9eaf79382e509063d27670642564ff7
LordPrinz <plazinski2902@gmail.com>
2023-04-03 23:12:01 +0200

    Create Dzajdrop Alpha

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 					name="keywords"
 					content="link, bitly, link shortener, shortener, dzajlopment, LordPrinz, dzajco, dzajcarz, bubr, dżajco, dżajcarz, zip file, Oskar Płaziński, short, short link, zsti, lordprinzteam, team, lpt, best link, best, best link shortener, de, dzajde, dzaj.de, dzaj de, dzaj de link, dzajde link, dzaj.de link, dzaj.de link short, dzajde link short, dzaj de link short, drop, dzajdrop, file, storage"
 				/>
-				<meta name="robots" content="index, follow" />
+				<meta name="robots" content="all" />
 				<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
 				<meta name="language" content="English" />
 				<meta name="author" content="LordPrinz" />
